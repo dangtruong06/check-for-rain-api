8aca5d282414b854baf1f056aa41b788c3b21e49
Tony Dang <DANGTT066@GMAIL.COM>
2026-04-30 21:43:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+load_dotenv()
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+api_key = os.getenv("OWM_API_KEY")
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+params = {
+    "lat" : 30.6,
+    "lon": 94.25,
+    "cnt": 4,
+    "units": "imperial",
+    "appid": api_key
+}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+response = requests.get('https://api.openweathermap.org/data/2.5/forecast', params=params)
+response.raise_for_status()
+data = response.json()
+li = data['list']
+rain = False
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+for i in li:
+    # if i['weather'][0]['id'] < 700:
+    condition_code = i['weather'][0]['id']
+    if condition_code < 700:
+        rain = True
+
+if rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="Hewo, it will rain on in the next 12 hours",
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+16572466100",
+    )
